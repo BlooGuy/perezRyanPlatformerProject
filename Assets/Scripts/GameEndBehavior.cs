158ad67f81081c68215b227119ae086c9b4979ba
BlooGuy <rfp0070@outlook.com>
2022-10-05 16:28:19 -0500

    perezRyanMilestone1

---
+++ Assets/Scripts/GameEndBehavior.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEndBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            ReloadGame();
+        }
+    }
+
+    private void ReloadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
     public float Speed;
+    private int jumpMax = 2; 
+    private int jumpBase = 0;
+    private bool noJump = false;
     private Vector2 newPosition;
     public Vector2 jumpForce = new Vector2(0, 300); //editable in inspector
 
@@ -18,14 +21,29 @@ public class PlayerBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        print(jumpBase + "/" + jumpMax);
         Vector2 newPosition = transform.position;
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb2d.velocity = Vector2.zero;
-            rb2d.AddForce(jumpForce);
+                bool shouldJump = (Input.GetKeyDown(KeyCode.Space));
 
+        if (shouldJump && !noJump) //! means "not" for  boolean
+         {
+            print("Jump");
+        //reduce velocity and then jump up
+        rb2d.velocity = Vector2.zero;
+        rb2d.AddForce(jumpForce);
+        jumpBase++;
         }
+        if (jumpBase == jumpMax)
+        {
+            noJump = true;
+        }
+
+        if (noJump == true)
+        {
+            shouldJump = false;
+        }
+   
         if (Input.GetKey(KeyCode.S))
         {
             newPosition += Vector2.down * Time.deltaTime * Speed;
@@ -40,5 +58,10 @@ public class PlayerBehavior : MonoBehaviour
         }
 
         transform.position = new Vector2(Mathf.Clamp(newPosition.x, -200, 200), newPosition.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumpBase = 0;
+        noJump = false;
     }
 }
